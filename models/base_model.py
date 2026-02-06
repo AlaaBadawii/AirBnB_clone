e23771b15a54ed2e0ce1026a0ac9aa166fba05c7
Alaa Badawii <alaa.badawy404@gmail.com>
2026-02-06 18:52:19 +0200

    CHECKER: problem

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Defines the BaseModel class."""
 import uuid
 from datetime import datetime
@@ -10,26 +10,32 @@ class BaseModel():
         """ initilizie a new BaseModel instance
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-    def __str__(self):
-        """
-        should print:
-        [<class name>] (<self.id>) <self.__dict__>
-        """
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__
-        )
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
-        """Public instance methods"""
-        dict = {**self.__dict__}  # id created_at updated_at
-        dict['__class__'] = type(self).__name__  # BaseModel
-        dict['created_at'] = dict['created_at'].isoformat()
-        dict['updated_at'] = dict['updated_at'].isoformat()
-        return dict
+        """returns a dictionary containing all keys/values"""
+        obj_dict = self.__dict__.copy()
+
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+
+        return obj_dict
 
     def save(self):
-        """Public instance methods"""
-        self.updated_at = datetime.now()
+        """ save:
+            updates the public instance attribute updated_at
+            with the current datetime
+        """
+        self.updated_at = datetime.utcnow()
+
+    def __str__(self):
+        """ __str__:
+            prints [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__,
+            self.id,
+            self.__dict__
+        )
