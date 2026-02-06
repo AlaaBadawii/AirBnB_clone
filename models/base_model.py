0ed9e7949ee9d9ea18aefcb22cc2571b90062d08
Alaa Badawii <alaa.badawy404@gmail.com>
2026-02-06 18:48:01 +0200

    CHECKER: problem

--- models/base_model.py
+++ models/base_model.py
@@ -10,25 +10,25 @@ class BaseModel():
         """ initilizie a new BaseModel instance
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-
-    def save(self):
-        """ save:
-            updates the public instance attribute updated_at
-            with the current datetime
-        """
-        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values"""
-        obj_dict = 'l'
+        obj_dict = self.__dict__.copy()
 
         obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
 
         return obj_dict
+
+    def save(self):
+        """ save:
+            updates the public instance attribute updated_at
+            with the current datetime
+        """
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """ __str__:
