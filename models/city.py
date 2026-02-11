ad7614e4e74094ffe4a619ea1b73d18fd2bc3cf7
Alaa Badawii <alaa.badawy404@gmail.com>
2026-02-11 21:46:03 +0200

    CRT: new classes State, City, Amenity, Place, Review

---
+++ models/city.py
@@ -0,0 +1,8 @@
+"""City class module"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class"""
+    state_id = ""
+    name = ""
---
+++ models/review.py
@@ -0,0 +1,9 @@
+"""State class module"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Review class"""
+    place_id = ""
+    user_id = ""
+    text = ""
