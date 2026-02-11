e03156bfd5e6c78bd8fa328179bd8a028dcedfd3
Alaa Badawii <alaa.badawy404@gmail.com>
2026-02-11 21:48:44 +0200

    UPD: console.py to manage correctly serialization and deserialization of all our new classes

--- console.py
+++ console.py
@@ -3,7 +3,12 @@
 import cmd
 from models.base_model import BaseModel
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.review import Review
+from models.state import State
 from models.user import User
+from models.place import Place
 
 
 class HBNBCommand(cmd.Cmd):
@@ -13,7 +18,12 @@ class HBNBCommand(cmd.Cmd):
 
     classes = {
         "BaseModel": BaseModel,
-        "User": User
+        "User": User,
+        "Amenity": Amenity,
+        "City": City,
+        "Review": Review,
+        "State": State,
+        "Place": Place
         }
 
     def do_create(self, arg):
